5e82c7f12680e6ceacb97ad5e9cce03fc8bac9f3
jackanderson12 <43770968+jackanderson12@users.noreply.github.com>
2021-11-30 07:27:39 -0500

    Finished Dicee App
    Added dice image views. Added a roll button. Created an array of dice images. Added button action of when pressed select a random integer corresponding to the array of dice images.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //IBOutlet allows you to reference a UI element
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    //The IBAction executes code when button is pressed
+    //Have to use #imageView( to display the image selector
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        //constant because we are not changing the array of die sides
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"  ), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        
     }
-
-
 }
 
